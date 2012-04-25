90a8eed6486a2cac4a4ec00bc51885d76544e2b3
jvelezpo <sebasvelez@gmail.com>
2012-04-24 22:36:48 -0500

    gemfile

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,4 +3,16 @@ class UserController < ApplicationController
     @title = "RMS-Solutions"
     @hola = Contactanos.first
   end
+
+  def create
+    @contacto = current_user.microposts.build(params[:micropost])
+    if @micropost.save
+      flash[:success] = "Comentario Creado!!!!"
+      redirect_to root_path
+    else
+      @feed_items = current_user.feed
+      render 'pages/home'
+    end
+
+  end
 end
