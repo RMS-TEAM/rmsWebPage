c417256433f95e856e0f358dd9722b819b10187f
James Benitez <michinbenitez@gmail.com>
2012-04-25 18:47:24 -0500

    cambios en la tabla

---
+++ app/controllers/comentarios_controller.rb
@@ -0,0 +1,24 @@
+class ComentariosController < ApplicationController
+  def index
+    @comentarios = User.all
+
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def delete
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rms::Application.routes.draw do
 
+  get "comentarios/index"
+
+  get "comentarios/show"
+
+  get "comentarios/new"
+
+  get "comentarios/create"
+
+  get "comentarios/update"
+
+  get "comentarios/destroy"
+
+  get "comentarios/delete"
+
    resources :users
    root :to => 'users#index'
 end
