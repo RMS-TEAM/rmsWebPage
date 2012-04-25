0867287c79664a9ef237f90b2b1ceb0ab4115b6b
jvelezpo <sebasvelez@gmail.com>
2012-04-24 22:52:03 -0500

    no db

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,18 +1,8 @@
 class UserController < ApplicationController
   def index
     @title = "RMS-Solutions"
-    @hola = Contactanos.first
-  end
-
-  def create
-    @contacto = current_user.microposts.build(params[:micropost])
-    if @micropost.save
-      flash[:success] = "Comentario Creado!!!!"
-      redirect_to root_path
-    else
-      @feed_items = current_user.feed
-      render 'pages/home'
-    end
 
   end
+
+
 end
