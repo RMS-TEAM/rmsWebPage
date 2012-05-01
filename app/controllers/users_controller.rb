ab926866d2767aeb31fc602652a11e7b0cc240e4
jvelezpo <sebasvelez@gmail.com>
2012-04-30 22:07:46 -0500

    cambios en los users de cada uno

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,18 +16,21 @@ class UsersController < ApplicationController
   end
 
   def rderoldan1
+    @title = "Ruben"
     @response1 = user_info('rderoldan1')
     @response =  user_repos('rderoldan1')
 
   end
 
   def jvelezpo
+    @title = "Sebastian"
     @response1 = user_info('jvelezpo')
     @response =  user_repos('jvelezpo')
 
   end
 
   def jvidalba1
+    @title = "Mateo"
     @response1 = user_info('jvidalba1')
     @response =  user_repos('jvidalba1')
     @response2= HTTParty.get("https://api.github.com/gists/921286")
